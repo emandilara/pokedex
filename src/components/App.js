04ca5cf1e3f0d8d4f1a322955ba9115963385010
Eleni Mandilara <madelinegr@gmail.com>
2020-11-10 14:23:16 +0100

    Add styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import Store from './store/Store'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import AppNavigation from './AppNavigation';
 import AppRouter from './AppRouter';
+import './styles/App.css';
 
 function App() {
   return (
     <div className='app'>
-      <AppNavigation />
-      <Container fluid className='app-container'>
-        <Row>
-          <Col xs={12} md={12}>
-            <AppRouter />
-          </Col>
-        </Row>
-      </Container>
+      <Store>
+        <AppNavigation />
+        <Container fluid className='app-container'>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <AppRouter />
+            </Col>
+          </Row>
+        </Container>
+      </Store>
     </div>
   );
 }
