3bd27c07b81f8ce189897211913e138c9f972c36
Eleni Mandilara <madelinegr@gmail.com>
2020-11-10 14:33:26 +0100

    Display Pokemon abilities

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import { Context } from './store/Store';
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -9,24 +10,27 @@ import Button from 'react-bootstrap/Button';
 import Loading from './Loading';
 import PokemonInfo from './PokemonInfo';
 
-class Pokedex extends Component {
-  
-  state = {
-    pokemons: [],
-    pokemonsFetched: false,
-    showingPokemonInfo: false
-  }
+const Pokedex = () => {
+
+  // Initialise local state
+  const [pokemons, setPokemons] = React.useState([]);
+  const [pokemonInfo, setPokemonInfo] = React.useState(undefined);
+  const [pokemonsFetched, setPokemonsFetched] = React.useState(false);
+  const [showingPokemonInfo, setShowingPokemonInfo] = React.useState(false);
+
+  // Use global state
+  const [myPokemonList, setMyPokemonList] = React.useContext(Context);
 
   // Filtering the pokemon list by name or number
-  filterPokemon = (event) => {
+  const filterPokemon = (event) => {
     const filterValue = event && event.target && event.target.value.toLowerCase();
-    this.setState({
-      pokemons: this.state.pokemons.filter(pokemon =>
+    setPokemons(
+      pokemons.filter(pokemon =>
         pokemon.name.toLowerCase().includes(filterValue) || pokemon.number === filterValue)
-    });
+    );
   }
 
-  extractPokemonNumber = (url) => {
+  const extractPokemonNumber = (url) => {
     // This Regexp is to match the pokemon number
     // However it also matches the version of the API - it can be improved
     const numberPattern = /\d+/g;
@@ -34,76 +38,81 @@ class Pokedex extends Component {
     return matches[1];
   }
 
-  addToPokemonList = (pokemon) => {
-    this.props.addToPokemonList(pokemon);
+  const addToPokemonList = (pokemon) => {
+    setMyPokemonList({type: 'ADD', payload: pokemon});
   }
 
-  showPokemonInfo = (pokemon) => {
-    this.setState({ ...this.state, showingPokemonInfo: true, pokemonInfo: pokemon });
+  const showPokemonInfo = (pokemon) => {
+    setShowingPokemonInfo(true);
+    setPokemonInfo(pokemon);
   }
 
-  componentDidMount() {
-    axios.get(`https://pokeapi.co/api/v2/pokemon`)
-      .then(res => {
-        const data = res && res.data.results;
-        const pokemons = data.map(pokemon => { return { ...pokemon, number: this.extractPokemonNumber(pokemon.url) } });
-        this.setState({ pokemons, pokemonsFetched: true });
-      })
-  }
+  // Fetch data when component mounts
+  React.useEffect(() => {
+    const fetchData = async () => {
+      axios.get(`https://pokeapi.co/api/v2/pokemon`)
+        .then(res => {
+          const data = res && res.data.results;
+          const pokemons = data.map(pokemon => { return { ...pokemon, number: extractPokemonNumber(pokemon.url) } });
+          setPokemons(pokemons);
+          setPokemonsFetched(true);
+        })
+    };
 
-  render() {
-    return (
-      <Fragment>
-        <h2>Pokedex - Gotta catch'em all!</h2>
-        <Container fluid>
-          <Row>
-            <Col xs={12} md={6} lg={6}>
-              {!this.state.pokemonsFetched && <Loading />}
-              {this.state.pokemonsFetched &&
-                <Fragment>                  
-                  <Form>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Control type="text"
-                        placeholder="Search for a pokemon"
-                        onChange={this.filterPokemon}
-                      />
-                    </Form.Group>
-                  </Form>
+    fetchData();
+  }, []);
 
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Add to my list</th>
+  return (
+    <Fragment>
+      <h2>Pokedex - Gotta catch'em all!</h2>
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={6} lg={6}>
+            {!pokemonsFetched && <Loading />}
+            {pokemonsFetched &&
+              <Fragment>
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="text"
+                      placeholder="Search for a pokemon"
+                      onChange={filterPokemon}
+                    />
+                  </Form.Group>
+                </Form>
+
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Add to my list</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {pokemons.map((pokemon, index) =>
+                      <tr key={index.toString()}>
+                        <td>{pokemon.number}</td>
+                        <td>
+                          <Button variant="outline-dark" onClick={() => showPokemonInfo(pokemon)}>
+                            {pokemon.name}
+                          </Button>
+                        </td>
+                        <td>
+                          <Button variant="dark" onClick={() => addToPokemonList(pokemon)}>+</Button>{' '}
+                        </td>
                       </tr>
-                    </thead>
-                    <tbody>
-                      {this.state.pokemons.map((pokemon, index) =>
-                        <tr key={index.toString()}>
-                          <td>{pokemon.number}</td>
-                          <td>
-                            <Button variant="outline-dark" onClick={() => this.showPokemonInfo(pokemon)}>
-                              {pokemon.name}
-                            </Button>
-                          </td>
-                          <td>
-                            <Button variant="dark" onClick={() => this.addToPokemonList(pokemon)}>+</Button>{' '}
-                          </td>
-                        </tr>
-                      )}
-                    </tbody>
-                  </Table>
-                </Fragment>}
-            </Col>
-            <Col xs={12} md={6} lg={6}>
-              {!!this.state.showingPokemonInfo && <PokemonInfo about={this.state.pokemonInfo} />}
-            </Col>
-          </Row>
-        </Container>
-      </Fragment>
-    )
-  }
+                    )}
+                  </tbody>
+                </Table>
+              </Fragment>}
+          </Col>
+          <Col xs={12} md={6} lg={6}>
+            {!!showingPokemonInfo && <PokemonInfo name={pokemonInfo.name} url={pokemonInfo.url} />}
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
+  )
 }
 
 export default Pokedex;
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -4,16 +4,23 @@ import axios from 'axios';
 import Loading from './Loading';
 
 const PokemonInfo = (props) => {
-  const { name, url } = { ...props.about };
+  const { name, url } = { ...props };
   const [info, setInfo] = React.useState({});
   const [infoFetched, setInfoFetched] = React.useState(false);
 
-  axios.get(url)
-    .then(res => {
-      const data = res && res.data;
-      setInfo(data);
-      setInfoFetched(true);
-    })
+  // Fetch data when component mounts
+  React.useEffect(() => {
+    const fetchData = async () => {
+      axios.get(url)
+        .then(res => {
+          const data = res && res.data;
+          setInfo(data);
+          setInfoFetched(true);
+        })
+    };
+
+    fetchData();
+  }, [name]);
 
   return (
     <Fragment>
@@ -21,11 +28,17 @@ const PokemonInfo = (props) => {
       { !!infoFetched &&
         <Card>
           <Card.Body>
-            <h3>{name}</h3>
+            <h3>{info.name}</h3>
             <h5>Order: {info.order}</h5>
             <h5>Height: {info.height}</h5>
             <h5>Weight: {info.weight}</h5>
             <h5>Base experience: {info.base_experience}</h5>
+            <h5>Abilities: </h5>
+            <ul>
+              { info.abilities && info.abilities.map((ability, index) =>
+                <li key={index}>{ ability.ability.name } <span>{ability.is_hidden && "(hidden)"}</span></li>)
+              }
+            </ul>
           </Card.Body>
         </Card>
       }
