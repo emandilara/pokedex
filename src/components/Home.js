1d2d1cde191c191183dac5f0159b09a15f490446
Eleni Mandilara <madelinegr@gmail.com>
2020-11-22 19:49:22 +0100

    Add link to Pokedex

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <br />
       <h5>Browse your favourite pokemon and make your own list :)</h5>
       <br />
-      <h5>Navigate to the "Pokedex" tab to start browsing.</h5>
+      <h5>Navigate to the <a href='/pokedex'>Pokedex</a> tab to start browsing.</h5>
       <h5>You can click on the name of a Pokemon to display its details.</h5>
       <h5>You can also add a Pokemon to your own list!</h5>
     </Jumbotron>
